69e4af0ac2458f2cb64fee370dcf55a7381a7d28
Machuca Juan <machucajuangabriel@gmail.com>
2022-04-26 14:58:19 -0300

    actualización

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/juanmachuca95/stream_golang
 
 go 1.17
 
-require gocv.io/x/gocv v0.30.0
+require (
+	github.com/hybridgroup/mjpeg v0.0.0-20140228234708-4680f319790e
+	gocv.io/x/gocv v0.30.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/hybridgroup/mjpeg"
 	"gocv.io/x/gocv"
 )
 
+var (
+	deviceID int
+	err      error
+	webcam   *gocv.VideoCapture
+	stream   *mjpeg.Stream
+)
+
 func main() {
-	webcam, _ := gocv.VideoCaptureDevice(0)
-	window := gocv.NewWindow("Hello")
+	if len(os.Args) < 3 {
+		fmt.Println("How to run:\n\tmjpeg-streamer [camera ID] [host:port]")
+		return
+	}
+
+	// parse args
+	deviceID := os.Args[1]
+	host := os.Args[2]
+
+	// open webcam
+	webcam, err = gocv.OpenVideoCapture(deviceID)
+	if err != nil {
+		fmt.Printf("Error opening capture device: %v\n", deviceID)
+		return
+	}
+	defer webcam.Close()
+
+	// create the mjpeg stream
+	stream = mjpeg.NewStream()
+
+	// start capturing
+	go mjpegCapture()
+
+	fmt.Println("Capturing. Point your browser to " + host)
+
+	// start http server
+	http.Handle("/", stream)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+func mjpegCapture() {
 	img := gocv.NewMat()
+	defer img.Close()
 
 	for {
-		webcam.Read(&img)
-		window.IMShow(img)
-		window.WaitKey(1)
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed: %v\n", deviceID)
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		buf, _ := gocv.IMEncode(".jpg", img)
+		stream.UpdateJPEG(buf.GetBytes())
+		buf.Close()
 	}
 }
